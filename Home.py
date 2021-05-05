fa1db11ab8c0fea11339d5499022432cad14c091
Hamza Rashid <hamza1886@gmail.com>
2021-05-05 14:13:27 +0500

    Added output field

--- Home.py
+++ Home.py
@@ -15,88 +15,95 @@ from PyQt5 import Qt, QtCore, QtGui, QtWidgets
 
 
 class Ui_HomeWindow(object):
-    def __init__(self, HomeWindow):
-        self.home_window = HomeWindow
+    def setup_ui(self, window):
+        self.home_window = window
+        if not self.home_window.objectName():
+            self.home_window.setObjectName("home_window")
 
-    def setup_ui(self):
-        self.home_window.setObjectName("home_window")
-        self.home_window.resize(859, 706)
+        self.home_window.resize(681, 536)
         self.central_widget = QtWidgets.QWidget(self.home_window)
         self.central_widget.setObjectName("central_widget")
 
-        font = QtGui.QFont()
-        font.setPointSize(12)
-
-        self.radio_button_1 = QtWidgets.QRadioButton(self.central_widget)
-        self.radio_button_1.setGeometry(QtCore.QRect(130, 260, 401, 31))
-        self.radio_button_1.setObjectName("radio_button_1")
-        self.radio_button_1.input_type = 'sequence'
-        self.radio_button_1.setChecked(True)
-        self.radio_button_1.toggled.connect(self.on_clicked)
-
-        self.input_path = QtWidgets.QLabel(self.central_widget)
-        self.input_path.setGeometry(QtCore.QRect(130, 460, 321, 51))
-        self.input_path.setObjectName("input_path")
-        self.input_path.setDisabled(True)
-
-        self.radio_button_2 = QtWidgets.QRadioButton(self.central_widget)
-        self.radio_button_2.setGeometry(QtCore.QRect(130, 440, 401, 31))
-        self.radio_button_2.setObjectName('radio_button_2')
-        self.radio_button_2.input_type = 'file'
-        self.radio_button_2.toggled.connect(self.on_clicked)
-
-        self.choose_input_file = QtWidgets.QPushButton(self.central_widget)
-        self.choose_input_file.setGeometry(QtCore.QRect(630, 460, 131, 31))
-        self.choose_input_file.setObjectName("choose_input_file")
-        self.choose_input_file.clicked.connect(self.choose_file)
-        self.choose_input_file.setDisabled(True)
-
-        self.encrypt_text = QtWidgets.QPushButton(self.central_widget)
-        self.encrypt_text.setGeometry(QtCore.QRect(270, 580, 131, 31))
-        self.encrypt_text.setObjectName("encrypt_text")
-        self.encrypt_text.clicked.connect(self.encrypt)
-        self.encrypt_text.setDisabled(True)
-
-        self.decrypt_text = QtWidgets.QPushButton(self.central_widget)
-        self.decrypt_text.setGeometry(QtCore.QRect(470, 580, 131, 31))
-        self.decrypt_text.setObjectName("decrypt_text")
-        self.decrypt_text.clicked.connect(self.decrypt)
-        self.decrypt_text.setDisabled(True)
-
-        self.clear_form = QtWidgets.QPushButton(self.central_widget)
-        self.clear_form.setGeometry(QtCore.QRect(130, 520, 131, 31))
-        self.clear_form.setObjectName("clear_form")
-        self.clear_form.clicked.connect(self.clear)
-        self.clear_form.setDisabled(True)
-
-        self.example_input = QtWidgets.QPushButton(self.central_widget)
-        self.example_input.setGeometry(QtCore.QRect(630, 250, 131, 31))
-        self.example_input.setObjectName("example_input")
-        self.example_input.clicked.connect(self.example)
-
         fixed_width_font = QtGui.QFont()
-        fixed_width_font.setPointSize(10)
+        fixed_width_font.setPointSize(8)
         fixed_width_font.setFamily('Courier New')
 
-        self.input_text = QtWidgets.QTextEdit(self.central_widget)
-        self.input_text.setGeometry(QtCore.QRect(130, 290, 641, 141))
-        self.input_text.setLineWrapMode(QtWidgets.QTextEdit.WidgetWidth)
-        self.input_text.setFont(fixed_width_font)
-        self.input_text.setObjectName("input_text")
-        self.input_text.textChanged.connect(self.on_text_changed)
-
         self.help_button = QtWidgets.QPushButton(self.central_widget)
-        self.help_button.setGeometry(QtCore.QRect(780, 10, 70, 23))
+        self.help_button.setGeometry(QtCore.QRect(580, 10, 80, 25))
         self.help_button.setObjectName("help_button")
         self.help_button.clicked.connect(self.help_button_click_handler)
 
+        self.example_input_button = QtWidgets.QPushButton(self.central_widget)
+        self.example_input_button.setGeometry(QtCore.QRect(550, 60, 111, 25))
+        self.example_input_button.setObjectName("example_input_button")
+        self.example_input_button.clicked.connect(self.example_input_button_click_handler)
+
+        self.radio_button_1 = QtWidgets.QRadioButton(self.central_widget)
+        self.radio_button_1.setGeometry(QtCore.QRect(20, 60, 401, 25))
+        self.radio_button_1.setObjectName("radio_button_1")
+        self.radio_button_1.input_type = 'sequence'
+        self.radio_button_1.setChecked(True)
+        self.radio_button_1.toggled.connect(self.radio_button_click_handler)
+
+        self.radio_button_2 = QtWidgets.QRadioButton(self.central_widget)
+        self.radio_button_2.setGeometry(QtCore.QRect(20, 250, 401, 25))
+        self.radio_button_2.setObjectName('radio_button_2')
+        self.radio_button_2.input_type = 'file'
+        self.radio_button_2.toggled.connect(self.radio_button_click_handler)
+
+        self.input_text = QtWidgets.QTextEdit(self.central_widget)
+        self.input_text.setGeometry(QtCore.QRect(20, 90, 641, 141))
+        self.input_text.setLineWrapMode(QtWidgets.QTextEdit.WidgetWidth)
+        self.input_text.setFont(fixed_width_font)
+        self.input_text.setObjectName("input_text")
+        self.input_text.textChanged.connect(self.input_text_changed_handler)
+
+        self.input_path = QtWidgets.QLabel(self.central_widget)
+        self.input_path.setGeometry(QtCore.QRect(20, 280, 641, 25))
+        self.input_path.setObjectName("input_path")
+
+        self.choose_file_button = QtWidgets.QPushButton(self.central_widget)
+        self.choose_file_button.setGeometry(QtCore.QRect(550, 250, 111, 25))
+        self.choose_file_button.setObjectName("choose_input_file")
+        self.choose_file_button.clicked.connect(self.choose_file_button_click_handler)
+        self.choose_file_button.setDisabled(True)
+
+        self.output_label = QtWidgets.QLabel(self.central_widget)
+        self.output_label.setGeometry(QtCore.QRect(20, 310, 641, 25))
+        self.output_label.setObjectName("output_label")
+
+        self.output_text = QtWidgets.QTextEdit(self.central_widget)
+        self.output_text.setGeometry(QtCore.QRect(20, 340, 641, 141))
+        self.output_text.setLineWrapMode(QtWidgets.QTextEdit.WidgetWidth)
+        self.output_text.setFont(fixed_width_font)
+        self.output_text.setObjectName("output_text")
+        self.output_text.setReadOnly(True)
+
+        self.clear_form_button = QtWidgets.QPushButton(self.central_widget)
+        self.clear_form_button.setGeometry(QtCore.QRect(20, 490, 111, 25))
+        self.clear_form_button.setObjectName("clear_form_button")
+        self.clear_form_button.clicked.connect(self.clear_form_button_click_handler)
+        self.clear_form_button.setDisabled(True)
+
+        self.encrypt_button = QtWidgets.QPushButton(self.central_widget)
+        self.encrypt_button.setGeometry(QtCore.QRect(420, 490, 111, 25))
+        self.encrypt_button.setObjectName("encrypt_button")
+        self.encrypt_button.clicked.connect(self.encrypt_button_click_handler)
+        self.encrypt_button.setDisabled(True)
+
+        self.decrypt_button = QtWidgets.QPushButton(self.central_widget)
+        self.decrypt_button.setGeometry(QtCore.QRect(550, 490, 111, 25))
+        self.decrypt_button.setObjectName("decrypt_button")
+        self.decrypt_button.clicked.connect(self.decrypt_button_click_handler)
+        self.decrypt_button.setDisabled(True)
+
         self.menu_bar = QtWidgets.QMenuBar(self.home_window)
-        self.menu_bar.setGeometry(QtCore.QRect(0, 0, 859, 20))
-        self.menu_bar.setObjectName("menubar")
+        self.menu_bar.setGeometry(QtCore.QRect(0, 0, 681, 20))
+        self.menu_bar.setObjectName("menu_bar")
         self.home_window.setMenuBar(self.menu_bar)
 
         self.status_bar = QtWidgets.QStatusBar(self.home_window)
-        self.status_bar.setObjectName("statusbar")
+        self.status_bar.setObjectName("status_bar")
         self.home_window.setStatusBar(self.status_bar)
 
         self.home_window.setCentralWidget(self.central_widget)
@@ -105,49 +112,52 @@ class Ui_HomeWindow(object):
 
     def translate_ui(self):
         _translate = QtCore.QCoreApplication.translate
-        self.home_window.setWindowTitle(_translate("home_window", "home_window"))
+
+        self.home_window.setWindowTitle(_translate("home_window", "Data Anonymization"))
+        self.help_button.setText(_translate("home_window", "Help"))
+        self.example_input_button.setText(_translate("home_window", "Example input"))
         self.radio_button_1.setText(_translate("home_window", "Input text to encrypt"))
         self.radio_button_2.setText(_translate("home_window", "Upload file from your computer"))
-        self.choose_input_file.setText(_translate("home_window", "Choose File"))
-        self.encrypt_text.setToolTip(_translate("home_window", "Click to encrypt data"))
-        self.encrypt_text.setText(_translate("home_window", "Encrypt"))
-        self.decrypt_text.setToolTip(_translate("home_window", "Click to decrypt data"))
-        self.decrypt_text.setText(_translate("home_window", "Decrypt"))
-        self.clear_form.setText(_translate("home_window", "Clear form"))
-        self.example_input.setText(_translate("home_window", "Example input"))
-        self.help_button.setText(_translate("home_window", "Help"))
+        self.choose_file_button.setText(_translate("home_window", "Choose File"))
+        self.output_label.setText(_translate("home_window", "Output"))
+        self.clear_form_button.setText(_translate("home_window", "Clear form"))
+        self.encrypt_button.setToolTip(_translate("home_window", "Click to encrypt data"))
+        self.encrypt_button.setText(_translate("home_window", "Encrypt"))
+        self.decrypt_button.setToolTip(_translate("home_window", "Click to decrypt data"))
+        self.decrypt_button.setText(_translate("home_window", "Decrypt"))
 
-    def on_text_changed(self):
+    def input_text_changed_handler(self):
         is_disabled = self.input_text.toPlainText().strip() == '' and self.input_path.text().strip() == ''
-        self.encrypt_text.setDisabled(is_disabled)
-        self.decrypt_text.setDisabled(is_disabled)
+        self.encrypt_button.setDisabled(is_disabled)
+        self.decrypt_button.setDisabled(is_disabled)
 
         has_content = self.input_text.toPlainText().strip() != '' or self.input_path.text().strip() != ''
-        self.clear_form.setDisabled(not has_content)
+        self.clear_form_button.setDisabled(not has_content)
 
-    def on_clicked(self):
+    def radio_button_click_handler(self):
         if self.radio_button_1.isChecked():
             self.input_text.setDisabled(False)
             self.input_path.setDisabled(True)
-            self.choose_input_file.setDisabled(True)
+            self.choose_file_button.setDisabled(True)
         elif self.radio_button_2.isChecked():
             self.input_text.setDisabled(True)
             self.input_path.setDisabled(False)
-            self.choose_input_file.setDisabled(False)
+            self.choose_file_button.setDisabled(False)
 
     def help_button_click_handler(self):
         # TODO: replace with GitHub wiki url
         url = Qt.QUrl('https://www.google.com')
         Qt.QDesktopServices.openUrl(url)
 
-    def example(self):
+    def example_input_button_click_handler(self):
         self.input_text.setText('hello')
 
-    def clear(self):
+    def clear_form_button_click_handler(self):
         self.input_text.setText('')
         self.input_path.setText('')
+        self.output_text.setText('')
 
-    def choose_file(self):
+    def choose_file_button_click_handler(self):
         filename, _filter = QtWidgets.QFileDialog.getOpenFileName(caption='Select .txt file containing input text', filter='*.txt')
         self.input_path.setText(filename)
 
@@ -159,7 +169,7 @@ class Ui_HomeWindow(object):
             sequence = f.read()
             self.input_text.setText(sequence)
 
-    def encrypt(self):
+    def encrypt_button_click_handler(self):
         scale = 16
         otp = ''
         num_of_bits = 8
@@ -203,9 +213,9 @@ class Ui_HomeWindow(object):
         hexadecimal = hex(n)
         hexadecimal = hexadecimal.replace('0x', '')
 
-        print('Hex value of ciphertext is', hexadecimal)
+        self.output_text.setText(hexadecimal)
 
-    def decrypt(self):
+    def decrypt_button_click_handler(self):
         self.window = QtWidgets.QMainWindow()
         # self.ui = Ui_SearchTemplate(self.window, seq_id)
         self.ui.setup_ui()
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import sys
+
 from PyQt5 import QtWidgets
 
 from Home import Ui_HomeWindow
 
-
 if __name__ == '__main__':
-    # input_str = input('Enter text to encrypt: ')
-    # encrypt_str(input_str)
-
     app = QtWidgets.QApplication(sys.argv)
-    HomeWindow = QtWidgets.QMainWindow()
-    ui = Ui_HomeWindow(HomeWindow)
-    ui.setup_ui()
-    HomeWindow.show()
-    HomeWindow.setWindowTitle('Data Anonymization')
+    home_window = QtWidgets.QMainWindow()
+    ui = Ui_HomeWindow()
+    ui.setup_ui(home_window)
+    home_window.show()
     sys.exit(app.exec())
